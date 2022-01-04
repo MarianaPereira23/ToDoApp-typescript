29382f86ac092f47c89f2447452845fe3cd09ba2
Mariana <mariana.c.pereira.23@hotmail.com>
2022-01-04 12:14:31 +0100

    Added mongodb connection and api endpoint to join and login

---
+++ server/app.ts
@@ -0,0 +1,52 @@
+import express from 'express';
+import cors from 'cors';
+import bcrypt from 'bcrypt';
+import { addUser, getUser } from './mongo';
+import { User, LoginUser } from './types';
+
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
+
+const hash = async (password: string) => {
+  const hashedPassword = await bcrypt.hash(password, 10);
+  return hashedPassword;
+}
+
+app.post('/join', async (req, res) => {
+  try {
+    const user: User = req.body.user;
+    const dbUser = await getUser(user.email);
+    if (dbUser != "Sorry, this user does not seem to exist.") {
+      res.status(400).send('Sorry, a user with that email address already exists')
+    }
+    user.password = await hash(user.password);
+    await addUser(user);
+    res.sendStatus(201);
+  } catch (err) {
+    res.sendStatus(500);
+  }
+});
+
+app.post('/login', async (req, res) => {
+  try {
+    const user: LoginUser = req.body.user;
+    const dbUser = await getUser(user.email);
+    if (dbUser === "Sorry, this user does not seem to exist.") {
+      res.status(404).send('Sorry, a user with that email address does not seem to exist');
+    } 
+    if (dbUser && dbUser != "Sorry, this user does not seem to exist."){
+      const validate: boolean = await bcrypt.compare(user.password, dbUser.password);
+      if (!validate) {
+        res.status(401).send('Sorry, something is wrong with your credentials')
+      }
+      res.status(200).send(dbUser.username);
+    }
+  } catch (err) {
+    res.sendStatus(500);
+  }
+})
+
+app.listen(8000, () => console.log('listening on 8000'));
---
+++ server/mongo.ts
@@ -0,0 +1,34 @@
+import { MongoClient } from 'mongodb';
+import { config } from 'dotenv';
+import { User } from './types';
+
+config();
+
+const uri: string = process.env.DB_CONNECTION ? process.env.DB_CONNECTION : '';
+const client: MongoClient = new MongoClient(uri);
+
+export const addUser = async (user: User) => {
+  try {
+    await client.connect();
+    await client.db("todo-typescript").collection("users").insertOne(user);
+    await client.close();
+    return "User added";
+  } catch (err) {
+    await client.close();
+    return "Sorry, there was an error adding this user, please try again later."
+  }
+};
+
+export const getUser = async (email: string) => {
+  try {
+    await client.connect();
+    const dbUser = await client.db("todo-typescript").collection("users").findOne({ email });
+    await client.close();
+    if (!dbUser) {
+      return "Sorry, this user does not seem to exist."
+    }
+    return dbUser;
+  } catch (err) {
+    console.log(err);
+  }
+};
