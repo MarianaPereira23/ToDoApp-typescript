9d4ce90bf363091a488040b90898c796a658d793
Mariana <mariana.c.pereira.23@hotmail.com>
2022-01-07 16:16:46 +0100

    removed old files

--- client/src/store/store.ts
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import todosReducer from "../slices/todoSlice";
-
-export const store = configureStore({
-  reducer: todosReducer,
-});
-
-export type MyDispatch = typeof store.dispatch;
-export type MyState = ReturnType<typeof store.getState>;
